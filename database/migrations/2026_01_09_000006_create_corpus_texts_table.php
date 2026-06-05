59cc7a38963161ed02891efa87c86d9be8f58f02
Molitor István <istvanmolitor@gmail.com>
2026-06-05 09:07:21 +0200

    migration

--- database/migrations/2026_01_09_000006_create_corpus_texts_table.php
+++ database/migrations/2026_01_09_000006_create_corpus_texts_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->text('text');
-            $table->text('tokens');
             $table->timestamps();
         });
     }
